18efcfef3f4b65287f8de8c01a74a95c0b9215df
Admin <awuyong@aliyun.com>
2023-08-05 09:53:50 +0800

    gitlab added

--- devops/gitlab/run.sh
+++ devops/gitlab/run.sh
@@ -1,9 +1,9 @@
 ## NFS
-mkdir -p /data/storage/kubernetes/gitlab/{config,logs,data}
-chmod 777 -R /data/storage/kubernetes/gitlab/ 
+mkdir -p /share/gitlab/{config,logs,data}
+chmod 777 -R /share/gitlab/
 
-mkdir -p /data/storage/kubernetes/gitlab-runner
-chmod 777 -R /data/storage/kubernetes/gitlab-runner
+mkdir -p /share/gitlab-runner
+chmod 777 -R /share/gitlab-runner
 
 ## Docker images
 docker pull gitlab/gitlab-ce:16.2.2-ce.0
@@ -14,4 +14,4 @@ kind load  docker-image gitlab/gitlab-ce:16.2.2-ce.0 --name devopscluster
 kind load  docker-image gitlab/gitlab-runner:alpine-v16.2.0 --name devopscluster
 
 ## ArgoAPP
-kubectl -n argocd apply -f gitlab-argoapp.yaml 
+kubectl -n argocd apply -f gitlab-argoapp.yaml
